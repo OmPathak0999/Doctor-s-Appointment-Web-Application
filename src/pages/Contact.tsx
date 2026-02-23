8712a416914acd7b803c62b9c2758a5352e95a6b
Om Sarvadeo Pathak <ompathak0999@gmail.com>
2026-02-23 19:57:14 +0530

    Update office address in Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -26,7 +26,7 @@ const Contact = () => {
               <h3 className="text-lg font-semibold text-muted-foreground mb-4">OUR OFFICE</h3>
               <div className="space-y-2 text-muted-foreground">
                 <p>24 Lane Near Victoria Ground</p>
-                <p> 24 A,Esplande, Kolkata</p>
+                <p> Esplande, Kolkata</p>
                 <p className="pt-2">Tel: +91 68886888</p>
                 <p>Email: prescripto@gmail.com</p>
               </div>
